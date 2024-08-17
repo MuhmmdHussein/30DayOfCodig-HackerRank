9883a81e98864f042b33782ecc557b998e973270
MuhmmdHussein <muhmmdamer@gmail.com>
2024-08-17 10:36:45 +0300

    #Day1

--- day1.py
+++ day1.py
@@ -1,3 +1,16 @@
-input_text = input()
-print("Hello, World.")
-print(input_text)
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+
+x = int(input())       
+y = float(input())     
+z = input() 
+
+sum_i = i + x                             
+sum_d = d + y                            
+concatenated_string = s + z
+
+print(sum_i)
+print(sum_d)
+print(concatenated_string)
